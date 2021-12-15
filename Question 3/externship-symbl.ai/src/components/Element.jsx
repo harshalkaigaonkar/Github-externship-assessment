25bb7a1163f10c3470b608f9ffcdabc56e46705f
harshalkaigaonkar <harshalkaigaonkar31@gmail.com>
2021-12-15 15:49:57 +0530

    Error Handlings

--- Question 3/externship-symbl.ai/src/components/Element.jsx
+++ Question 3/externship-symbl.ai/src/components/Element.jsx
@@ -1,23 +1,32 @@
 import React, { useEffect, useState } from 'react'
-import Loading from './Loading';
-import Items from './Items';
+import Loading from './Loading'
+import Items from './Items'
 
 const Element = () => {
     const [data, setData] = useState(null)
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
     useEffect(() => {
-        async function fetchData() {
-            const res = await fetch("http://localhost:3001/get-data").then(res => res.json())
-            res.data.sort((a, b) => a.id-b.id)
-            setData(res.data);
-            setLoading(false);
+        function fetchData() {
+            fetch("http://localhost:3001/get-data")
+            .then(res => res.json())
+            .then(res => {
+                if(res.data) {
+                    res.data.sort((a, b) => a.id-b.id)
+                    setData(res.data)
+                    setLoading(false)
+                } else {
+                    setError('Server Error')
+                }
+            })
+            .catch(err => setError(`Error due to ${err.message}`))
         }
         fetchData();
-    }, [setData, setLoading])
+    }, [setData, setLoading, setError])
     return (
         <>
             {
-                loading ? <Loading /> : <Items data={data}/>
+                loading ? <Loading error={error} /> : <Items data={data}/>
             }
         </>
     )
--- Question 3/externship-symbl.ai/src/components/Loading.jsx
+++ Question 3/externship-symbl.ai/src/components/Loading.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Loading = () => {
+const Loading = ({ error }) => {
     return (
         <div className='img_container'> 
             <img src='loading.gif' alt='Loading...' className='img_main' />
+            <p>{error && error}</p>
         </div>
     )
 }
